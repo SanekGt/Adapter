a26de47ae1ef051bfd4f20ca53ad8712ca206076
sanekGt <sanek275892@gmail.com>
2019-05-31 11:28:05 +0300

    First commit

---
+++ Adapter/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Adapter
+{
+
+    // Mainapp test application 
+
+    class MainApp
+    {
+        static void Main()
+        {
+            // Create adapter and place a request 
+            Target target = new Adapter();
+            target.Request();
+
+            // Wait for user 
+            Console.Read();
+        }
+    }
+
+    // "Target" 
+
+    class Target
+    {
+        public virtual void Request()
+        {
+            Console.WriteLine("Called Target Request()");
+        }
+    }
+
+    // "Adapter" 
+
+    class Adapter : Target
+    {
+        private Adaptee adaptee = new Adaptee();
+
+        public override void Request()
+        {
+            // Possibly do some other work 
+            // and then call SpecificRequest 
+            adaptee.SpecificRequest();
+            adaptee.SpecificRequest2();
+        }
+    }
+
+    // "Adaptee" - адаптируемый класс
+
+    class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("Called SpecificRequest()");
+        }
+
+        public void SpecificRequest2()
+        {
+            Console.WriteLine("Called SpecificRequest2()");
+        }
+    }
+}
+
